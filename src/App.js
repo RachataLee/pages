0b56acccc1d77c899f42132118221cebb153be1d
RachataLee <taza009@live.com>
2018-01-19 16:06:52 +0700

    เพิ่ม Router

--- src/App.js
+++ src/App.js
@@ -2,12 +2,30 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import _ from 'lodash'
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
 
 const students = [
   {id: 12,name: 'Johny Walker' , score: 23},
   {id: 15,name: 'Jack Danial' , score: 26},
   {id: 17,name: 'Obiwan Kennobi' , score: 28},  
 ]
+
+const Home = () => (<div>Home</div>)
+
+const Students = () => (
+  <div>
+    
+      students
+    
+    
+  </div>
+)
+
+
 
 const StudentLine = (props) => (
   <div>{props.id} {props.name} = {props.score}</div>
@@ -17,7 +35,12 @@ class App extends Component {
     return (
       <div>
         {
-          _.map(students,student=> (<StudentLine {...student} />))
+          <Router>
+            <div>
+              <Route exact path="/" component={Home}/>
+              <Route path="/students" component={Students}/> 
+            </div>       
+        </Router>
         }
       </div>
     );
