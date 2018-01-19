cc8cdd68a614ff77531a2d6396b49aec18fb713e
RachataLee <taza009@live.com>
2018-01-19 15:36:54 +0700

    โชว์ score

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import BasicExample from './BasicExample'
+import _ from 'lodash'
 
+const students = [
+  {id: 12,name: 'Johny Walker' , score: 23},
+  {id: 15,name: 'Jack Danial' , score: 26},
+  {id: 17,name: 'Obiwan Kennobi' , score: 28},  
+]
+
+const StudentLine = (props) => (
+  <div>{props.id} {props.name} = {props.score}</div>
+)
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <BasicExample/>
+      <div>
+        {
+          _.map(students,student=> (<StudentLine {...student} />))
+        }
       </div>
     );
   }
